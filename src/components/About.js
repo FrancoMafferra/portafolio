ef00b12ade5ee1a1e4e378a6e47c3adf850b95c7
francoMafferra <francomafferra38@gmail.com>
2023-08-17 19:22:21 -0300

    formato y bandeja gmail

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import { useInView } from 'react-intersection-observer';
- 
+import React from "react";
+// import { useInView } from 'react-intersection-observer';
+
 const About = () => {
   const [ref, inView] = useInView({ threshold: 0.5 });
-  
+
   return (
-  <section className='section' id='about' ref={ref}>
-  <div className='container mx-auto flex flex-col lg:flex-row items-center'>
-  <div className='lg:w-1/2'>
-  <div className='bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></div>
-  </div>
-  <div className='lg:w-1/2 lg:pl-10'>
-  <div>
-  <h2 className='font-primary text-sm tracking-[2px]'>About me.</h2>
-  <h3 className='h3 mb-4'>I'm a Freelance Full-stack Developer with over 1 year of experience.</h3>
-  <p className='mb-6'>
-  As a full stack developer, I am passionate about creating innovative and functional technology solutions. Throughout my career, I've worked with a wide range of technologies, including Vue.js, React, React Native, Node.js, Express.js, and jQuery. In addition, I have experience managing relational and non-relational databases, which allows me to work on projects of varying complexity.
-  </p>
-  <p>
-  I am a dedicated and committed professional, always looking for new experiences and challenges that allow me to continue growing in my career. My main focus is to provide efficient and quality solutions to my clients, collaborating closely with them to understand their needs and objectives.
-  </p>
-  </div>
-  <button className='btn btn-lg'>Contact me</button>
-  </div>
-  </div>
-  </section>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto flex flex-col lg:flex-row items-center">
+        <div className="lg:w-1/2">
+          <div className="bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+        </div>
+        <div className="lg:w-1/2 lg:pl-10">
+          <div>
+            <h2 className="font-primary text-sm tracking-[2px]">About me.</h2>
+            <h3 className="h3 mb-4">
+              I'm a Freelance Full-stack Developer with over 1 year of
+              experience.
+            </h3>
+            <p className="mb-6">
+              As a full stack developer, I am passionate about creating
+              innovative and functional technology solutions. Throughout my
+              career, I've worked with a wide range of technologies, including
+              Vue.js, React, React Native, Node.js, Express.js, and jQuery. In
+              addition, I have experience managing relational and non-relational
+              databases, which allows me to work on projects of varying
+              complexity.
+            </p>
+            <p>
+              I am a dedicated and committed professional, always looking for
+              new experiences and challenges that allow me to continue growing
+              in my career. My main focus is to provide efficient and quality
+              solutions to my clients, collaborating closely with them to
+              understand their needs and objectives.
+            </p>
+          </div>
+          <button className="btn btn-lg">Contact me</button>
+        </div>
+      </div>
+    </section>
   );
-  };
+};
 export default About;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,65 +1,64 @@
-import React from 'react';
-import Image from '../assets/avatar.png';
-import {FaGithub, } from 'react-icons/fa';
+import React from "react";
+import Image from "../assets/avatar.png";
+import { FaGithub } from "react-icons/fa";
 import { AiFillLinkedin } from "react-icons/ai";
-import {TypeAnimation} from 'react-type-animation'
-import {motion} from 'framer-motion'
-import {fadeIn} from '../variants'
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-  <section className='section' id='home'>
-  <div className='container mx-auto flex items-center justify-center'>
-  <div className='w-2/3 lg:w-1/3'>
-  
-  <div className='flex-1 text-center font-secondary lg:text-left'>
-  <h1 className='text-5xl font-bold leading-tight lg:text-9xl'>
-  FRANCO <span>MAFFERRA</span>
-  </h1>
-  </div>
-  
-  <div className='text-4xl lg:text-6xl font-secondary font-semibold leading-relaxed mb-6'>
-  <span className='text-white mr-4'>I am a</span>
-  <TypeAnimation
-  repeat={Infinity}
-  speed={50}
-  className='text-accent'
-  wrapper='span'
-  sequence={[
-  'DEVELOPER',
-  2000,
-  'FULL-STACK DEVELOPER!',
-  2000,
-  ]}
-  />
-  </div>
-  
-  <p className='text-lg mb-6'>
-  In addition to my technical background, I consider myself a committed and dedicated professional. I love working as a team and collaborating closely with my clients to understand their needs and offer tailor-made solutions. My main goal is to create quality digital experiences that are visually appealing and easy to use.
-  </p>
-  
-  <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
-  <button className='btn btn-lg'>Contact me</button>
-  <a href='#' className='text-gradient btn-link'>My Portfolio</a>
-  </div>
-  
-  <div className='flex text-2xl gap-x-6 max-w-max mx-auto lg:mx-0 mt-3'>
-  <a href='https://www.linkedin.com/in/franco-mafferra-82a5a1248/'>
-  <AiFillLinkedin target='blank' color='#fff' />
-  </a>
-  <a href='https://github.com/FrancoMafferra'>
-  <FaGithub target='blank' color='#fff' />
-  </a>
-  </div>
-  
-  </div>
-  
-  <div className='hidden lg:flex flex-1 justify-center '>
-  <img src={Image} alt='' className='w-full' />
-  </div>
-  </div>
-  </section>
+    <section className="section" id="home">
+      <div className="container mx-auto flex items-center justify-center">
+        <div className="w-2/3 lg:w-1/3">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-5xl font-bold leading-tight lg:text-9xl">
+              FRANCO <span>MAFFERRA</span>
+            </h1>
+          </div>
+
+          <div className="text-4xl lg:text-6xl font-secondary font-semibold leading-relaxed mb-6">
+            <span className="text-white mr-4">I am a</span>
+            <TypeAnimation
+              repeat={Infinity}
+              speed={50}
+              className="text-accent"
+              wrapper="span"
+              sequence={["DEVELOPER", 2000, "FULL-STACK DEVELOPER!", 2000]}
+            />
+          </div>
+
+          <p className="text-lg mb-6">
+            In addition to my technical background, I consider myself a
+            committed and dedicated professional. I love working as a team and
+            collaborating closely with my clients to understand their needs and
+            offer tailor-made solutions. My main goal is to create quality
+            digital experiences that are visually appealing and easy to use.
+          </p>
+
+          <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            <button className="btn btn-lg">Contact me</button>
+            <a href="#" className="text-gradient btn-link">
+              My Portfolio
+            </a>
+          </div>
+
+          <div className="flex text-2xl gap-x-6 max-w-max mx-auto lg:mx-0 mt-3">
+            <a href="https://www.linkedin.com/in/franco-mafferra-82a5a1248/">
+              <AiFillLinkedin target="blank" color="#fff" />
+            </a>
+            <a href="https://github.com/FrancoMafferra">
+              <FaGithub target="blank" color="#fff" />
+            </a>
+          </div>
+        </div>
+
+        <div className="hidden lg:flex flex-1 justify-center ">
+          <img src={Image} alt="" className="w-full" />
+        </div>
+      </div>
+    </section>
   );
-  }; 
+};
 
 export default Banner;
